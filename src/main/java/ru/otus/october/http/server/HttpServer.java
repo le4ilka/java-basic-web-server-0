7ad4e1f6b987f0b4d53e3ddc399fc0e056e5f59e
olga <romanovar-151@ya.ru>
2024-10-14 14:17:22 +0300

    добавлен non-stop для сервера, пул потоков и логгирование

--- src/main/java/ru/otus/october/http/server/HttpServer.java
+++ src/main/java/ru/otus/october/http/server/HttpServer.java
@@ -1,12 +1,20 @@
 package ru.otus.october.http.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
+
+    private static final Logger LOGGER = LogManager.getLogger(HttpServer.class);
+
 
     public HttpServer(int port) {
         this.port = port;
@@ -15,17 +23,36 @@ public class HttpServer {
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                request.info(true);
-                dispatcher.execute(request, socket.getOutputStream());
+            LOGGER.info("Сервер запущен на порту: {}", port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                ExecutorService executorService = Executors.newFixedThreadPool(10);
+                executorService.execute(() -> {
+                    try {
+                        LOGGER.info("Thread: {} ", Thread.currentThread().getName());
+                        fromRequestToDispatcher(socket);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+                executorService.shutdown();
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void fromRequestToDispatcher(Socket socket) throws IOException {
+        byte[] buffer = new byte[8192];
+        int n = socket.getInputStream().read(buffer);
+        if (n == -1) {
+            return;
+        }
+        String rawRequest = new String(buffer, 0, n);
+        HttpRequest request = new HttpRequest(rawRequest);
+        request.info(false);
+        dispatcher.execute(request, socket.getOutputStream());
     }
 }
